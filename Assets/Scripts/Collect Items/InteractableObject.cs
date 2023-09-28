543be2b4830c125dad49a65dad107c730ec60e2a
LamPhi101102 <124247193+LamPhi101102@users.noreply.github.com>
2023-09-28 14:14:17 +0700

    Allow Player open 3 chests

--- Assets/Scripts/Collect Items/InteractableObject.cs
+++ Assets/Scripts/Collect Items/InteractableObject.cs
@@ -6,6 +6,12 @@ using Debug = UnityEngine.Debug;
 
 public class InteractableObject : MonoBehaviour
 {
+    private Animator animator;
+
+    public GameObject chestLocked;
+    public GameObject chestOpen;
+
+    public float delay = 3.0f;
 
     public bool playerInRange;
     public string ItemName;
@@ -21,21 +27,44 @@ public class InteractableObject : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.F) && playerInRange && SelectionManager.Instance.onTarget && SelectionManager.Instance.selectedObject == gameObject)
         {
             // check if the inventory is not full
-            if (!InventorySystem.instance.CheckIfFull())
+            if (!gameObject.CompareTag("CommonChest") && !gameObject.CompareTag("ExquisiteChest") && !gameObject.CompareTag("PreciousChest") && !InventorySystem.instance.CheckIfFull())
             {
-
                 InventorySystem.instance.AddToInventory(ItemName);
                 Destroy(gameObject);
+            }
+            else if (gameObject.CompareTag("CommonChest"))
+            {
+                chestLocked.SetActive(false);
+                chestOpen.SetActive(true);
+                Destroy(chestOpen, 3.0f);
+                Destroy(chestLocked, 3.0f);
+            }
+            else if (gameObject.CompareTag("ExquisiteChest"))
+            {
+                chestLocked.SetActive(false);
+                chestOpen.SetActive(true);
+                Destroy(chestOpen, 3.0f);
+                Destroy(chestLocked, 3.0f);
+            }
+            else if (gameObject.CompareTag("PreciousChest"))
+            {
+                chestLocked.SetActive(false);
+                chestOpen.SetActive(true);
+                Destroy(chestOpen, 3.0f);
+                Destroy(chestLocked, 3.0f);
             }
             else
             {
                 Debug.Log("Inventory is full");
-            }
-            
-
+            }          
         }
     }
 
+    public void DeleteObjectAfterDelay(float delay)
+    {
+        // Use the Destroy function with the specified delay.
+        Destroy(chestOpen, delay);
+    }
 
     private void OnTriggerEnter(Collider other)
     {
