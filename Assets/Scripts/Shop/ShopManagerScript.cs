6cbc86796b15543a4e9beb3cce0c0185df3f0220
LamPhi101102 <124247193+LamPhi101102@users.noreply.github.com>
2023-10-10 15:38:29 +0700

    Add more icon package and add  new items in shops

--- Assets/Scripts/Shop/ShopManagerScript.cs
+++ Assets/Scripts/Shop/ShopManagerScript.cs
@@ -7,17 +7,27 @@ using UnityEngine.UI;
 
 public class ShopManagerScript : MonoBehaviour
 {
-    public int[,] shopItems = new int[13, 13];
-    public string[,] shopItemsResource = new string[13, 13];
+    public int[,] shopItems = new int[25, 25];
+    public string[,] shopItemsResource = new string[25, 25];
     public float silvers;
     public float golds;
+    public GameObject shopUI;
+    public GameObject shopPage1;
+    public GameObject shopPage2;
 
     public TextMeshProUGUI coinsSilversTxt;
-	//public TextMeshProUGUI coinsGoldsTxt;
-
+    //public TextMeshProUGUI coinsGoldsTxt;
+    Button NextPage;
+    Button PreviousPage;
 
     void Start()
     {
+        NextPage = shopUI.transform.Find("Content").transform.Find("NextPage").GetComponent<Button>();
+        NextPage.onClick.AddListener(delegate { NextToPage(); });
+
+        PreviousPage = shopUI.transform.Find("Content2").transform.Find("PreviousPage").GetComponent<Button>();
+        PreviousPage.onClick.AddListener(delegate { PreviousToPage(); });
+
         coinsSilversTxt.text = "Coins: " + silvers.ToString();
         //coinsGoldsTxt.text = "Coins: " + golds.ToString();
 
@@ -34,6 +44,18 @@ public class ShopManagerScript : MonoBehaviour
         shopItems[1, 10] = 10;
         shopItems[1, 11] = 11;
         shopItems[1, 12] = 12;
+        shopItems[1, 13] = 13;
+        shopItems[1, 14] = 14;
+        shopItems[1, 15] = 15;
+        shopItems[1, 16] = 16;
+        shopItems[1, 17] = 17;
+        shopItems[1, 18] = 18;
+        shopItems[1, 19] = 19;
+        shopItems[1, 20] = 20;
+        shopItems[1, 21] = 21;
+        shopItems[1, 22] = 22;
+        shopItems[1, 23] = 23;
+        shopItems[1, 24] = 24;
 
         shopItemsResource[1, 8] = "Axe";
 
@@ -50,8 +72,32 @@ public class ShopManagerScript : MonoBehaviour
         shopItems[2, 10] = 100;
         shopItems[2, 11] = 110;
         shopItems[2, 12] = 120;
+        shopItems[2, 13] = 130;
+        shopItems[2, 14] = 140;
+        shopItems[2, 15] = 150;
+        shopItems[2, 16] = 160;
+        shopItems[2, 17] = 170;
+        shopItems[2, 18] = 180;
+        shopItems[2, 19] = 190;
+        shopItems[2, 20] = 200;
+        shopItems[2, 21] = 210;
+        shopItems[2, 22] = 220;
+        shopItems[2, 23] = 230;
+        shopItems[2, 24] = 240;
 
     }
+
+    private void NextToPage()
+    {
+        shopPage1.SetActive(false);
+        shopPage2.SetActive(true);
+    }
+    private void PreviousToPage()
+    {
+        shopPage1.SetActive(true);
+        shopPage2.SetActive(false);
+    }
+
     void Update()
     {
         
