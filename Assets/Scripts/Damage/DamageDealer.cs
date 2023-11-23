c666d0bcf047c2b1b329b80c78f14d45789e9ba6
LamPhi101102 <124247193+LamPhi101102@users.noreply.github.com>
2023-11-24 01:58:15 +0700

    Add Enemy

--- Assets/Scripts/Damage/DamageDealer.cs
+++ Assets/Scripts/Damage/DamageDealer.cs
@@ -21,8 +21,8 @@ public class DamageDealer : MonoBehaviour
         if (canDealDamage)
         {
             RaycastHit hit;
-            int layerMask = 1 << 9;
-            if (Physics.Raycast(transform.position, -transform.up, out hit, weaponLength, layerMask))
+            int layerMask = 1 << 8;
+            if (Physics.Raycast(transform.position, -transform.forward, out hit, -weaponLength, layerMask))
             {
                 /*if (hit.transform.TryGetComponent(out Enemy enemy) && !hasDealtDamage.Contains(hit.transform.gameObject))
                 {
@@ -30,12 +30,13 @@ public class DamageDealer : MonoBehaviour
                     enemy.HitVFX(hit.point);
                     hasDealtDamage.Add(hit.transform.gameObject);
                 }*/
-                if (!hasDealtDamage.Contains(hit.transform.gameObject))
+                if (hit.transform.TryGetComponent(out Enemy enemy) && !hasDealtDamage.Contains(hit.transform.gameObject))
                 {
                     Debug.Log("Damage");
+                    enemy.TakeDamage(weaponDamage);
                     hasDealtDamage.Add(hit.transform.gameObject);
                 }
-            }
+            }        
         }
     }
     public void StartDealDamage()
